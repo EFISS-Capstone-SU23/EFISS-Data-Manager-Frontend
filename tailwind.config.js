f0fda703ac136993143a00b9c7a8cad5199cdc43
DAN3002 <dinhanh300229@gmail.com>
2023-07-16 18:19:49 +0700

    Init eslint and husky

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	content: ['./src/**/*.{js,jsx,ts,tsx}' /* src folder, for example */ ],
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	safelist: [
 		'w-64',
 		'w-1/2',
@@ -14,40 +14,40 @@ module.exports = {
 		'leading-9',
 		'shadow-lg',
 		'bg-opacity-50',
-		'dark:bg-opacity-80'
+		'dark:bg-opacity-80',
 	],
-	darkMode: "class",
+	darkMode: 'class',
 	theme: {
 		extend: {
 			colors: {
 				primary: {
-					"50": "#eff6ff",
-					"100": "#dbeafe",
-					"200": "#bfdbfe",
-					"300": "#93c5fd",
-					"400": "#60a5fa",
-					"500": "#3b82f6",
-					"600": "#2563eb",
-					"700": "#1d4ed8",
-					"800": "#1e40af",
-					"900": "#1e3a8a"
-				}
+					50: '#eff6ff',
+					100: '#dbeafe',
+					200: '#bfdbfe',
+					300: '#93c5fd',
+					400: '#60a5fa',
+					500: '#3b82f6',
+					600: '#2563eb',
+					700: '#1d4ed8',
+					800: '#1e40af',
+					900: '#1e3a8a',
+				},
 			},
 			fontFamily: {
-				'sans': ['Inter', 'ui-sans-serif', 'system-ui', '-apple-system', 'system-ui', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'],
-				'body': ['Inter', 'ui-sans-serif', 'system-ui', '-apple-system', 'system-ui', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'],
-				'mono': ['ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', 'Liberation Mono', 'Courier New', 'monospace']
+				sans: ['Inter', 'ui-sans-serif', 'system-ui', '-apple-system', 'system-ui', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'],
+				body: ['Inter', 'ui-sans-serif', 'system-ui', '-apple-system', 'system-ui', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'],
+				mono: ['ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', 'Liberation Mono', 'Courier New', 'monospace'],
 			},
 			transitionProperty: {
-				'width': 'width'
+				width: 'width',
 			},
 			textDecoration: ['active'],
 			minWidth: {
-				'kanban': '28rem'
+				kanban: '28rem',
 			},
 		},
 	},
 	plugins: [
-		require('flowbite/plugin')
+		require('flowbite/plugin'),
 	],
 };
