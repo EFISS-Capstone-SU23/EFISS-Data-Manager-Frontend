0ef44abf9e5d4356ef28e52e28e57d4c6076838b
DAN3002 <dinhanh300229@gmail.com>
2023-07-16 17:44:55 +0700

    Change to base React App

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
-import './style.css';
-import 'flowbite/dist/flowbite.js';
-import './sidebar';
-import './charts';
-import './dark-mode';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-// Have the courage to follow your heart and intuition.
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
