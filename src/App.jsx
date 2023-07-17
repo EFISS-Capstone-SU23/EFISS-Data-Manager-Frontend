3ce459b8b62d5d8b7685269f0cb1dcfeff5f243a
DAN3002 <dinhanh300229@gmail.com>
2023-07-17 15:41:52 +0700

    Init React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Dashboard from './components/pages/dashboard/Dashboard';
+import Template from './components/pages/tenplate/Template';
+
 import NavBar from './components/NavBar';
 import SideBar from './components/SideBar';
-import Dashboard from './components/Dashboard';
 
 function App() {
 	return (
-		<>
+		<BrowserRouter>
 			<NavBar />
 			<div className="flex pt-16 overflow-hidden bg-gray-50 dark:bg-gray-900">
 				<SideBar />
@@ -13,11 +17,14 @@ function App() {
 					className="relative w-full h-full overflow-y-auto bg-gray-50 lg:ml-64 dark:bg-gray-900"
 				>
 					<main>
-						<Dashboard />
+						<Routes>
+							<Route path="/" element={<Dashboard />} />
+							<Route path="/template" element={<Template />} />
+						</Routes>
 					</main>
 				</div>
 			</div>
-		</>
+		</BrowserRouter>
 	);
 }
 
---
+++ src/components/pages/tenplate/Template.jsx
@@ -0,0 +1,9 @@
+function Template() {
+	return (
+		<div className="px-4 pt-6">
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, impedit dolorum qui rerum dolores numquam quod illo incidunt ab, vero, minima sed harum porro a quia facilis illum doloribus quae.
+		</div>
+	);
+}
+
+export default Template;
