46e48818ce49c15393d8c9adf43830c4677ac9dc
DAN3002 <dinhanh300229@gmail.com>
2023-07-19 03:14:19 +0700

    Change to fontawsome

--- src/components/pages/tenplate/TemplatePage.jsx
+++ src/components/pages/tenplate/TemplatePage.jsx
@@ -1,6 +1,10 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import { useState, useEffect } from 'react';
 import moment from 'moment';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+	faPlus, faEdit, faTrash, faChevronLeft, faChevronRight,
+} from '@fortawesome/free-solid-svg-icons';
 
 const dummyData = [
 	{
@@ -60,18 +64,7 @@ function TemplatePage() {
 								data-modal-toggle="add-user-modal"
 								className="inline-flex items-center justify-center w-1/2 px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 sm:w-auto dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
 							>
-								<svg
-									className="w-5 h-5 mr-2 -ml-1"
-									fill="currentColor"
-									viewBox="0 0 20 20"
-									xmlns="http://www.w3.org/2000/svg"
-								>
-									<path
-										fillRule="evenodd"
-										d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
-										clipRule="evenodd"
-									/>
-								</svg>
+								<FontAwesomeIcon icon={faPlus} className="w-4 h-4 mr-2" />
 								Add user
 							</button>
 						</div>
@@ -160,19 +153,7 @@ function TemplatePage() {
 													data-modal-toggle="edit-user-modal"
 													className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
 												>
-													<svg
-														className="w-4 h-4 mr-2"
-														fill="currentColor"
-														viewBox="0 0 20 20"
-														xmlns="http://www.w3.org/2000/svg"
-													>
-														<path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
-														<path
-															fillRule="evenodd"
-															d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
-															clipRule="evenodd"
-														/>
-													</svg>
+													<FontAwesomeIcon icon={faEdit} className="w-4 h-4 mr-2" />
 													Edit user
 												</button>
 												<button
@@ -180,18 +161,7 @@ function TemplatePage() {
 													data-modal-toggle="delete-user-modal"
 													className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-800 focus:ring-4 focus:ring-red-300 dark:focus:ring-red-900"
 												>
-													<svg
-														className="w-4 h-4 mr-2"
-														fill="currentColor"
-														viewBox="0 0 20 20"
-														xmlns="http://www.w3.org/2000/svg"
-													>
-														<path
-															fillRule="evenodd"
-															d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-															clipRule="evenodd"
-														/>
-													</svg>
+													<FontAwesomeIcon icon={faTrash} className="w-4 h-4 mr-2" />
 													Delete user
 												</button>
 											</td>
@@ -224,18 +194,7 @@ function TemplatePage() {
 						href="#"
 						className="inline-flex items-center justify-center flex-1 px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
 					>
-						<svg
-							className="w-5 h-5 mr-1 -ml-1"
-							fill="currentColor"
-							viewBox="0 0 20 20"
-							xmlns="http://www.w3.org/2000/svg"
-						>
-							<path
-								fillRule="evenodd"
-								d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-								clipRule="evenodd"
-							/>
-						</svg>
+						<FontAwesomeIcon icon={faChevronLeft} className="w-4 h-4 mr-1 -ml-1" />
 						Previous
 					</a>
 					<a
@@ -243,18 +202,7 @@ function TemplatePage() {
 						className="inline-flex items-center justify-center flex-1 px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
 					>
 						Next
-						<svg
-							className="w-5 h-5 ml-1 -mr-1"
-							fill="currentColor"
-							viewBox="0 0 20 20"
-							xmlns="http://www.w3.org/2000/svg"
-						>
-							<path
-								fillRule="evenodd"
-								d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-								clipRule="evenodd"
-							/>
-						</svg>
+						<FontAwesomeIcon icon={faChevronRight} className="w-4 h-4 ml-1 -mr-1" />
 					</a>
 				</div>
 			</div>
