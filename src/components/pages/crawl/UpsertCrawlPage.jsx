1ff110c49513f14032ff30ba395d75e0114e6f13
DAN3002 <dinhanh300229@gmail.com>
2023-07-22 01:41:26 +0700

    Fetch template data in case create new crawl

--- src/components/pages/crawl/UpsertCrawlPage.jsx
+++ src/components/pages/crawl/UpsertCrawlPage.jsx
@@ -1,10 +1,21 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import CodeEditor from '@uiw/react-textarea-code-editor';
 
 import Breadcrumb from '../../forms/Breadcrumb';
 import { CrawIcon } from '../../../icons';
 import { codeEditorStyle } from '../../../config';
+import { getTemplateByID } from '../../../api/templateAPI';
 
 function UpsertCrawlPage() {
+	const { id } = useParams();
+	const isCreate = id === 'new';
+
+	const [template, setTemplate] = useState({});
+	const [website, setWebsite] = useState();
+	const [ignoreUrlPatterns, setIgnoreUrlPatterns] = useState([]);
+	const [numInstance, setNumInstance] = useState(1);
+
 	const breadcrumbList = [
 		{
 			text: 'Crawl',
@@ -12,9 +23,33 @@ function UpsertCrawlPage() {
 			icon: CrawIcon,
 		},
 		{
-			text: 'Start new crawl',
+			text: isCreate ? 'Start new crawl' : `Settings for crawl ${id}`,
 		},
 	];
+
+	const fetchTemplate = (templateId) => {
+		getTemplateByID(templateId)
+			.then((res) => {
+				const { template: templateData, website: websiteData } = res.data.template;
+
+				setIgnoreUrlPatterns(templateData.ignoreUrlPatterns);
+				delete templateData.ignoreUrlPatterns;
+				setTemplate(templateData);
+				setWebsite(websiteData);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+
+	useEffect(() => {
+		if (isCreate) {
+			// get templateId query in url
+			const urlParams = new URLSearchParams(window.location.search);
+			const templateId = urlParams.get('templateId');
+			fetchTemplate(templateId);
+		}
+	}, []);
 
 	return (
 		<>
@@ -22,7 +57,11 @@ function UpsertCrawlPage() {
 			<div className="px-4">
 				<div className="grid grid-cols-3 gap-4">
 					<div className="p-4 mb-4 bg-white border border-gray-200 rounded-lg shadow-sm">
-						<h3 className="mb-4 text-xl font-semibold">Template Data</h3>
+						<h3 className="mb-4 text-xl font-semibold">
+							Template Data -
+							{' '}
+							{website}
+						</h3>
 
 						<div
 							data-color-mode="light"
@@ -37,6 +76,7 @@ function UpsertCrawlPage() {
 								style={codeEditorStyle}
 								minHeight="25rem"
 								disabled
+								value={JSON.stringify(template, null, 4)}
 							/>
 						</div>
 					</div>
@@ -59,6 +99,7 @@ function UpsertCrawlPage() {
 								padding={15}
 								style={codeEditorStyle}
 								minHeight="25rem"
+								value={JSON.stringify(ignoreUrlPatterns, null, 4)}
 							/>
 						</div>
 					</div>
@@ -77,9 +118,11 @@ function UpsertCrawlPage() {
 									type="number"
 									name="num-instance"
 									className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
+									min="1"
+									value={numInstance}
+									onChange={(e) => setNumInstance(e.target.value)}
 								/>
 
-								{/* 3 button save, resume and stop */}
 								<div className="flex justify-end mt-4">
 									<button
 										type="button"
@@ -89,13 +132,15 @@ function UpsertCrawlPage() {
 									</button>
 									<button
 										type="button"
-										className="inline-flex justify-center px-4 py-2 ml-3 text-sm font-medium text-white bg-orange-400 border border-transparent rounded-md hover:bg-orange-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
+										className="inline-flex justify-center px-4 py-2 ml-3 text-sm font-medium text-white bg-orange-400 border border-transparent rounded-md hover:bg-orange-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500 disabled:opacity-50"
+										disabled={isCreate}
 									>
 										Resume
 									</button>
 									<button
 										type="button"
-										className="inline-flex justify-center px-4 py-2 ml-3 text-sm font-medium text-white bg-red-600 border border-transparent rounded-md hover:bg-red-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
+										className="inline-flex justify-center px-4 py-2 ml-3 text-sm font-medium text-white bg-red-600 border border-transparent rounded-md hover:bg-red-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500 disabled:opacity-50"
+										disabled={isCreate}
 									>
 										Stop
 									</button>
