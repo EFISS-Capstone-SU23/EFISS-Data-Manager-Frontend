ab906cf54e73d480d0cdb006d8a396ebe8a49f5e
DAN3002 <dinhanh300229@gmail.com>
2023-08-26 01:25:09 +0700

    feat: add toggle user float menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,14 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function NavBar() {
+	const [navbarOpen, setNavbarOpen] = useState(false);
+
+	const toggleNavbar = () => {
+		setNavbarOpen(!navbarOpen);
+	};
+
 	return (
 		<nav
 			className="fixed z-30 w-full bg-white border-b border-gray-200 h-15"
@@ -31,17 +38,50 @@ function NavBar() {
 								<button
 									type="button"
 									className="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300"
-									id="user-menu-button-2"
-									aria-expanded="false"
-									data-dropdown-toggle="dropdown-2"
+									onClick={toggleNavbar}
 								>
-									<span className="sr-only">Open user menu</span>
 									<img
 										className="w-8 h-8 rounded-full"
-										src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+										src="/images/user-avatar.png"
 										alt="user"
 									/>
 								</button>
+							</div>
+
+							<div
+								className={`z-50 my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600 ${navbarOpen ? 'block' : 'hidden'}`}
+								style={{
+									position: 'absolute',
+									inset: '0px auto auto 0px',
+									margin: '0px',
+									transform: 'translate(1714px, 55px)',
+								}}
+							>
+								<div className="px-4 py-3" role="none">
+									<p
+										className="text-sm text-gray-900 dark:text-white"
+										role="none"
+									>
+										Neil Sims
+									</p>
+									<p
+										className="text-sm font-medium text-gray-900 truncate dark:text-gray-300"
+										role="none"
+									>
+										neil.sims@flowbite.com
+									</p>
+								</div>
+								<ul className="py-1" role="none">
+									<li>
+										<a
+											href="#"
+											className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
+											role="menuitem"
+										>
+											Sign out
+										</a>
+									</li>
+								</ul>
 							</div>
 						</div>
 					</div>
