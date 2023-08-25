ef11e93abbde31e09d4afb84aef7d7f93faa91ba
DAN3002 <dinhanh300229@gmail.com>
2023-08-25 19:54:09 +0700

    feat: Login and set cookie

---
+++ src/api/authAPI.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const AUTH_SERVICE_URL = process.env.REACT_APP_AUTH_SERVICE_URL || 'http://localhost:3001';
+
+const instance = axios.create({
+	baseURL: AUTH_SERVICE_URL,
+	withCredentials: true,
+});
+
+export default {
+	login: (username, password) => {
+		return instance.post('/sign-in', {
+			username,
+			password,
+		});
+	},
+};
--- src/components/pages/auth/LoginPage.jsx
+++ src/components/pages/auth/LoginPage.jsx
@@ -1,31 +1,109 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
+import ModalManager from '../../../utils/ModalManager';
+import authAPI from '../../../api/authAPI';
+
+const validate = (username, password) => {
+	// check not empty
+	if (!username) {
+		return 'Username is required.';
+	}
+	if (!password) {
+		return 'Password is required.';
+	}
+
+	return null;
+};
 
 function LoginPage() {
+	const navigate = useNavigate();
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+
+	const handleLogin = () => {
+		const error = validate(username, password);
+		if (error) {
+			ModalManager.showError(error);
+			return;
+		}
+
+		authAPI
+			.login(username, password)
+			.then((res) => {
+				if (res.status === 200) {
+					ModalManager.showSuccess('Login successfully.');
+					navigate('/');
+				} else {
+					ModalManager.showError('Login failed.');
+				}
+			})
+			.catch((err) => {
+				ModalManager.showError(err.response?.data?.message || 'Login failed.');
+			});
+	};
+
 	return (
 		<main className="bg-gray-50 dark:bg-gray-900">
 			<div className="flex flex-col items-center justify-center px-6 pt-8 mx-auto md:h-screen pt:mt-0 dark:bg-gray-900">
-				<Link to="/" className="flex items-center justify-center mb-8 text-2xl font-semibold lg:mb-10 dark:text-white">
-					<img src="/images/logo.svg" className="mr-4 h-11" alt="FlowBite Logo" />
+				<Link
+					to="/"
+					className="flex items-center justify-center mb-8 text-2xl font-semibold lg:mb-10"
+				>
+					<img
+						src="/images/logo.svg"
+						className="mr-4 h-11"
+						alt="FlowBite Logo"
+					/>
 					<span>EFISS</span>
 				</Link>
-				{/* Card */}
 				<div className="w-full max-w-xl p-6 space-y-8 sm:p-8 bg-white rounded-lg shadow dark:bg-gray-800">
-					<h2 className="text-2xl font-bold text-gray-900 dark:text-white">
+					<h2 className="text-2xl font-bold text-gray-900">
 						Sign in to platform
 					</h2>
 					<form className="mt-8 space-y-6" action="#">
 						<div>
-							<label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-							<input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="name@company.com" required />
+							<label
+								htmlFor="username"
+								className="block mb-2 text-sm font-medium text-gray-900"
+							>
+								Your username
+							</label>
+							<input
+								type="username"
+								name="username"
+								className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
+								placeholder="Enter your username"
+								value={username}
+								onChange={(e) => setUsername(e.target.value)}
+							/>
 						</div>
 						<div>
-							<label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
-							<input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required />
+							<label
+								htmlFor="password"
+								className="block mb-2 text-sm font-medium text-gray-900"
+							>
+								Your password
+							</label>
+							<input
+								type="password"
+								name="password"
+								placeholder="Enter your password"
+								className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
+								value={password}
+								onChange={(e) => setPassword(e.target.value)}
+							/>
 						</div>
 						{/* <div className="flex items-start">
 							<a href="#" className="ml-auto text-sm text-primary-700 hover:underline dark:text-primary-500">Lost Password?</a>
 						</div> */}
-						<button type="submit" className="w-full px-5 py-3 text-base font-medium text-center text-white bg-primary-700 rounded-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 sm:w-auto dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Login to your account</button>
+						<button
+							type="button"
+							className="w-full px-5 py-3 text-base font-medium text-center text-white bg-primary-700 rounded-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 sm:w-auto"
+							onClick={handleLogin}
+						>
+							Login to your account
+						</button>
 					</form>
 				</div>
 			</div>
